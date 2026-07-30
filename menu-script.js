62d1954a73d82a051c4e0e213b7d693eb37ffeef
neerajsml1978-alt <neerajsml1978@gmail.com>
2026-07-30 15:38:44 +0530

    Update menu-script.js

--- menu-script.js
+++ menu-script.js
@@ -7,19 +7,41 @@ function toggleNav() {
   }
 }
 
+// ड्रॉपडाउन को खोलने और बंद करने के लिए फंक्शन
+function toggleDropdown(event) {
+  event.preventDefault();
+  var dropdownContent = document.getElementById("rashifalDropdown");
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+}
+
 // मेनू लोड करने के लिए फंक्शन
 function loadMenu() {
   const menuHTML = `
     <div id="mySideNav" class="sidenav">
       <a href="javascript:void(0)" class="closebtn" onclick="toggleNav()">&times;</a>
-      <a href="./rashifal/mesh.html">मेष राशि का भविष्यफल</a>
-      <a href="./rashifal/vrash.html">वृषभ राशि का भविष्यफल</a>
-      <a href="./rashifal/mithun.html">मिथुन राशि का भविष्यफल</a>
-      <a href="./rashifal/kark.html">कर्क राशि का भविष्यफल</a>
-      <a href="./rashifal/singh.html">सिंह राशि का भविष्यफल</a>
-      <a href="./rashifal/kanya.html">कन्या राशि का भविष्यफल</a>
-      <a href="./rashifal/tula.html">तुला राशि का भविष्यफल</a>
-      <a href="./rashifal/upay.html" >आज के अचूक उपाय</a>
+      
+      <!-- ड्रॉपडाउन मेनू (सभी राशियों का राशिफल) -->
+      <a href="#" class="dropdown-btn" onclick="toggleDropdown(event)">सभी राशियों का राशिफल &#9662;</a>
+      <div id="rashifalDropdown" class="dropdown-container">
+        <a href="./rashifal/mesh.html">मेष राशि</a>
+        <a href="./rashifal/vrash.html">वृषभ राशि</a>
+        <a href="./rashifal/mithun.html">मिथुन राशि</a>
+        <a href="./rashifal/kark.html">कर्क राशि</a>
+        <a href="./rashifal/singh.html">सिंह राशि</a>
+        <a href="./rashifal/kanya.html">कन्या राशि</a>
+        <a href="./rashifal/tula.html">तुला राशि</a>
+        <a href="./rashifal/vrischika.html">वृश्चिक राशि</a>
+        <a href="./rashifal/dhanu.html">धनु राशि</a>
+        <a href="./rashifal/makar.html">मकर राशि</a>
+        <a href="./rashifal/kumbh.html">कुंभ राशि</a>
+        <a href="./rashifal/meen.html">मीन राशि</a>
+      </div>
+
+      <a href="./rashifal/upay.html">आज के अचूक उपाय</a>
       <a href="./rashifal/navgrah.html">नवग्रह मंत्र</a>
       <a href="./rashifal/panchang.html">आज का पंचांग</a>
       <a href="./rashifal/gurupurnima.html">गुरु पूर्णिमा 2026</a>
@@ -31,3 +53,5 @@ function loadMenu() {
 
 // पेज लोड होते ही मेनू लोड करें
 window.onload = loadMenu;
+
+
